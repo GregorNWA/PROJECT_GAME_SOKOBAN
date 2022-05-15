76de246c96d0219ada714c06f27f0b37080cb23b
Gregor Walz <g.walz@hm.edu>
2022-05-15 16:59:07 +0200

    Automatic creation of Objects

--- src/Map.java
+++ src/Map.java
@@ -74,45 +74,57 @@ public class Map {
     public static void main(String[] args) {
         //Note: Add new Player with Position from Matrix, not manual positions)
         String[][] MAP = {
-               //0,1,2,3,4,5
-                {S,S,G,S,S,S},//0
-                {G,G,G,G,C,S},//1
-                {G,G,M,G,C,S},//2
-                {S,G,M,G,S,S},//3
-                {S,G,C,G,S,S},//4
-                {S,S,P,S,S,G},//5
+                //0,1,2,3,4,5
+                {S, S, C, S, S, C},//0
+                {G, C, G, G, C, S},//1
+                {G, G, M, G, G, S},//2
+                {S, G, M, G, S, S},//3
+                {S, G, C, G, S, C},//4
+                {S, S, P, S, S, G},//5
         };     //0,1,2,3,4,5
 
-        Map MAP1 = new Map(6,6);
+        Map MAP1 = new Map(6, 6);
         MAP1.setElements(MAP);
 
-        Player Player1 = new Player(5,2, false);
-
-        List<Crate> Crates = new ArrayList<Crate>();
-        Crates.add(new Crate(1,4,false));
-        Crates.add(new Crate(2,4,false));
-        Crates.add(new Crate(4,2,false));
-
-        //Find Crate out of List
-        for (Crate crate : Crates) {
-            if (crate.getColPos() == 1) {
-                crate.IsOnMark=true;
+        //Creating new Player depending on "P" in Matrix
+        Player Player1 = null;
+        for (int i = 0; i < MAP1.getRows(); i++) {
+            for (int j = 0; j < MAP1.getColumns(); j++) {
+                if (MAP1.getSingleElement(i, j) == P) {
+                    Player1 = new Player(i, j, false);
+                }
             }
         }
+        System.out.println("Player Position"+Player1.getRowPos()+","+Player1.getColPos());
+
+        //Createing new Crates depending on the "C" in the Matrix
+        List<Crate> Crates = new ArrayList<Crate>();
+        for (int i = 0; i < MAP1.getRows(); i++) {
+            for (int j = 0; j < MAP1.getColumns(); j++) {
+                if (MAP1.getSingleElement(i, j) == C) {
+                    Crates.add(new Crate(i, j, false));
+                }
+            }
+        }
+
+        for (Crate crate : Crates){
+            System.out.println(" Row "+crate.getRowPos()+" COL:"+ crate.colPos);
+        }
+
         System.out.println(MAP1);
-            //Up Click
-            //Find Crate
-            MAP1.MoveUp(Player1, MAP1, Crates);
+        //Up Click
+        //Find Crate
+        MAP1.MoveUp(Player1, MAP1, Crates);
 
-            System.out.println(MAP1);
-            MAP1.MoveUp(Player1, MAP1, Crates);
-            System.out.println(MAP1);
-            MAP1.MoveUp(Player1, MAP1, Crates);
-            System.out.println(MAP1);
-            MAP1.MoveUp(Player1, MAP1, Crates);
-            System.out.println(MAP1);
+        System.out.println(MAP1);
+        MAP1.MoveUp(Player1, MAP1, Crates);
+        System.out.println(MAP1);
+        MAP1.MoveUp(Player1, MAP1, Crates);
+        System.out.println(MAP1);
+        MAP1.MoveUp(Player1, MAP1, Crates);
+        System.out.println(MAP1);
 
-    //Change Player Position
+        //Change Player Position
     }
     public void MoveUp(Player PlayUp, Map Map, List<Crate> Crates)  {
 
